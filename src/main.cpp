550184d0e7066c77582a68dddc38640ceeffb8d2
Jack <78124447+JackDerksen@users.noreply.github.com>
2026-01-23 12:06:02 -0700

    Temporary sanity check test logic for state and hashing

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,63 @@
 #include "astar.h"
 
 int main(int argc, char** argv) {
-    size_t n = 3;
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <n>" << std::endl;
+        return 1;
+    }
+    size_t n = std::stoi(argv[1]);
+    size_t num_large = n * n + 1;
+
+    // Read large disks
+    std::vector<int> large(num_large);
+    for (size_t i = 0; i < num_large; ++i) {
+        std::cin >> large[i];
+    }
+
+    // Read small disks
+    std::vector<int> small(num_large);
+    for (size_t i = 0; i < num_large; ++i) {
+        std::cin >> small[i];
+    }
+
+    // Everything below is just a sanity test for StateHash (using dummy data).
+    // We can delete this later, of course :)
+    //
+    // If any of you want to test this yourself, I did so with:
+    // echo -e "1 2 3 4 5 6 7 8 9 10\n1 2 3 4 5 6 7 8 9 0" | ./bin/AB 3
+    State s1;
+    s1.large = {1,2,3};
+    s1.small = {4,5};
+
+    State s2 = s1;
+
+    State s3;
+    s3.large = {1,2};
+    s3.small = {4,5,6};
+    StateHash hasher;
+
+    std::cout << "Hash s1: " << hasher(s1) << std::endl;
+    std::cout << "Hash s2: " << hasher(s2) << std::endl;
+    std::cout << "Hash s3: " << hasher(s3) << std::endl;
+
+    if (hasher(s1) != hasher(s2)) {
+        std::cout << "Error: identical states have different hashes" << std::endl;
+        return 1;
+    }
+    if (hasher(s1) == hasher(s3)) {
+        std::cout << "Error: different states have same hash" << std::endl;
+        return 1;
+    }
+    std::cout << "Printing s1: " << s1 << std::endl;
+
     Problem problem(n);
     ZeroHeuristic h;
     AStar solver(problem, h);
 
     State start_state;
+    start_state.large = large;
+    start_state.small = small;
     auto solution = solver.solve(start_state);
+
     // print_solution(solution);
 }
