4e5a3f025584d0e9e06d3259cd37e16c94990300
Rordon <rordyc@gmail.com>
2026-02-02 10:14:00 -0700

    Added case for n=0 to run tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include "astar.h"
 #include "common.h"
 #include "problem.h"
+#include <cmath>
+#include <cstdint>
 
-Problem ReadUserInput(size_t n)
+Problem ReadUserInput(size_t num_large)
 {
-    size_t num_large = n * n + 1;
     // Read large disks
     std::vector<Disk> large(num_large);
     for (size_t i = 0; i < num_large; ++i) {
@@ -17,6 +18,7 @@ Problem ReadUserInput(size_t n)
         std::cin >> small[i];
     }
 
+	uint16_t n = floor(sqrt((double)num_large));
 	return Problem(large, small, n);
 }
 
@@ -26,7 +28,7 @@ void PrintSolution(std::vector<State> solution)
     for (State s : solution) { std::cout << s << std::endl; }
 }
 
-void Tests()
+void RunTests()
 {
     // Everything below is just a sanity test for StateHash (using dummy data).
     // We can delete this later, of course :)
@@ -72,23 +74,38 @@ int main(int argc, char** argv) {
     if (argc < 2)
 	{
         std::cerr << "Usage: " << argv[0] << " <n>" << std::endl;
-		// TODO: Different compiler flags for building with test input vs stdin.
-        // return 1;
+        return 1;
     }
 
-	// Read problem from stdin.
-    // size_t n = std::stoi(argv[1]);
-	// Problem p = ReadUserInput(n);
-	
-	// Problem from the example solver.
-	Problem p = Problem({1,3,4,2,4,3,2,1,1,2,3,4,3,1,4,2,5}, {1,1,1,1,2,2,2,2,3,4,3,3,4,0,4,3,4}, 4);
-	
-	// NOTE(rordon): Don't try this problem until we have a better heuristic. This one gets up to over 300,000,000 nodes with BFS.
-	//Problem p = Problem({1,3,3,4,1,2,1,2,5,4,2,3,3,2,4,1,4}, {0,2,4,1,3,4,2,3,1,4,3,2,1,3,4,1,2}, 4);
+	//NOTE(rordon): I'm making it so if you pass n as 0 it will run tests.
+    size_t n = std::stoi(argv[1]);
+	if (n > 0)
+	{
+		// Read problem from stdin.
+		Problem p = ReadUserInput(n);
 
-    ZeroHeuristic zeroHeuristic;
-    AStar solver(p, zeroHeuristic);
+		ZeroHeuristic zeroHeuristic;
+		AStar solver(p, zeroHeuristic);
 
-    auto solution = solver.solve();
-    PrintSolution(solution);
+		auto solution = solver.solve();
+		PrintSolution(solution);
+	}
+	else
+	{
+		RunTests();
+
+		// Problem from the example solver.
+		Problem p = Problem({1,3,4,2,4,3,2,1,1,2,3,4,3,1,4,2,5}, {1,1,1,1,2,2,2,2,3,4,3,3,4,0,4,3,4}, 4);
+		
+		// NOTE(rordon): Don't try this problem until we have a better heuristic. This one gets up to over 300,000,000 nodes with BFS.
+		//Problem p = Problem({1,3,3,4,1,2,1,2,5,4,2,3,3,2,4,1,4}, {0,2,4,1,3,4,2,3,1,4,3,2,1,3,4,1,2}, 4);
+
+		ZeroHeuristic zeroHeuristic;
+		AStar solver(p, zeroHeuristic);
+
+		auto solution = solver.solve();
+		PrintSolution(solution);
+	}
+
+	return 0;
 }
